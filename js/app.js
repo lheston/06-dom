c270d789836518f927b4aec79b5a9f9bf486b9ec
lheston <loomisheston@hotmail.com>
2016-06-29 16:02:27 -0700

    updated per class work

--- js/app.js
+++ js/app.js
@@ -1,3 +1,58 @@
 'use strict';
 
+
+window.onload = function() {
 /* your script goes here */
+/*
+console.log('Hello World!');
+
+window.setTimeout(function() {
+    console.log("Boo!");
+}, 5000);
+
+//for animation
+window.requestAnimationFrame(renderCallback);
+
+*/
+
+/*var links = document.getElementsByTagName('a');
+var len = links.length;
+
+for(var i=0; i<len; i++)
+{
+   links[i].setAttribute("target","_blank"); //sets attribute instead cannot be passed directly fyi
+}
+*/
+
+function blink(){
+    var header = document.querySelector('header');
+    header.classList.toggle('black');
+}
+
+window.setInterval(blink, 500);
+
+/*
+var np = document.createElement('p');
+undefined
+np.textContent="hello world"
+"hello world"
+np
+<p>​hello world​</p>​
+var h = document.querySelector('header')
+undefined
+h.appendChild(np)
+<p>​hello world​</p>​
+*/
+
+var img = document.querySelector('img');
+
+function handleClick(event){
+    console.log('clicky clicky');
+    console.log(event);
+    var elem = event.target;
+    console.log(elem);
+    elem.setAttribute('src','img/husky.jpg');
+}
+
+img.addEventListener('click', handleClick);
+}
--- js/warmup.js
+++ js/warmup.js
@@ -5,6 +5,9 @@ var testString = "99 luftballons / auf ihrem weg zum horizont";
 
 
 //Convert the test string into an array of words. Log out the results
+var words = testString.split(" ");
+console.log(words);
+
 
 
 //Write a function 'capitalize' that takes in a String and 
@@ -12,15 +15,47 @@ var testString = "99 luftballons / auf ihrem weg zum horizont";
 //Reference: http://www.w3schools.com/jsref/jsref_obj_string.asp
 //be sure and test your function!
 
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
 
+console.log(toTitleCase(testString));
+
+//notes
+var capitalize =  function(string){
+    var firstLetter = string.charAt(0).toUpperCase();
+    var rest = string.slice(1);
+    return firstLetter + rest;
+}
+
+console.log(capitalize("loomis"));
 
 //Now pass each value in your words array to the capitalize function by using the 
 //.map() function, and save the result to a new variable
 
+var capWords = words.map(capitalize);
 
+console.log(capWords);
 
 //Using .map() with an ANONYMOUS FUNCTION, add the text 
 //"Give me a " to each element in your capitalized array. 
 //Save the result to a new var (e.g., "cheer") and log it out
 
 
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, function(txt){return "Give me a " + txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+var cheer = toTitleCase(testString);
+
+console.log(cheer);
+
+//comments
+
+var cheer = capWords.map(function(string){
+    return "Give me a " + string;
+});
+
+console.log(cheer);
